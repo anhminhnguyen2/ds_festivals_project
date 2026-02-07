c068dea42b6f761c76ff8e6723e430fe36fc89aa
Anh Minh Nguyen <amnnguyen02@gmail.com>
2026-02-06 21:26:06 -1000

    added playwright script

--- spotify_api.py
+++
@@ -1,8 +0,0 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="YOUR_ID", client_secret="YOUR_SECRET"))
-
-artist_uri = 'spotify:artist:4y7D92uRM7070S4Y70O9Xv' # Example: Anyma
-artist = sp.artist(artist_uri)
-print(f"Current Popularity: {artist['popularity']}")
